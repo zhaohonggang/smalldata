0e50a3f41447355cf097c4eddf55e5afb6a72f40
zhaohonggang <zhaohonggang@yahoo.com>
2017-05-09 16:08:04 -0400

    clean up

--- static/housing/housing.build.js
+++ static/housing/housing.build.js
@@ -61752,7 +61752,7 @@ module.exports={render:function (){var _vm=this;var _h=_vm.$createElement;var _c
 },staticRenderFns: [function (){var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;
   return _c('div', [_c('h2', {
     staticClass: "category-menu-title"
-  }, [_vm._v("\n    Google map here111sss")])])
+  }, [_vm._v("\n    Google map here")])])
 }]}
 module.exports.render._withStripped = true
 if (false) {
